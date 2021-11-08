49cb650c39cbf484a1400c67594524197f2ea107
coolchaeyoung <ggodchae@naver.com>
2021-11-08 17:23:54 +0900

    config: Initial setting

---
+++ src/js/backend/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening at http://localhost:${PORT}`));
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { app: './src/js/frontend/app.js' },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'js/[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/template/index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: 'css/style.css' }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src/js')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              [
+                '@babel/plugin-transform-runtime',
+                {
+                  corejs: 3,
+                  proposals: true,
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    open: true,
+    port: 'auto',
+    proxy: {
+      '/todos': {
+        target: 'http://localhost:3000/todos',
+        pathRewrite: { '^/todos': '' },
+      },
+    },
+  },
+  devtool: 'source-map',
+  mode: 'development',
+};
