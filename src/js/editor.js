78105186e0a54b41bab8649237a94e588fa2c3cf
coolchaeyoung <ggodchae@naver.com>
2021-11-16 13:42:55 +0900

    feat: Add implements editor

--- src/js/editor.js
+++ src/js/editor.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 import Quill from 'quill';
 import rater from 'rater-js';
 
-const $form = document.querySelector('form');
+let ratings = 0;
 
-const quill = new Quill('.editor', {
+const $title = document.querySelector('.editor-title');
+const $tagList = document.querySelector('.tag-list');
+
+const quill = new Quill('#quill', {
   modules: {
     toolbar: [
       ['bold', 'italic'],
@@ -24,31 +27,56 @@ const myRater = rater({
   starSize: 24,
   rateCallback: (rating, done) => {
     myRater.setRating(rating);
+    ratings = rating;
     done();
   },
 });
 
-$form.addEventListener('submit', async e => {
-  e.preventDefault();
+document.querySelector('.submit').addEventListener('click', async () => {
+  if ($title.value.trim() === '') {
+    alert('제목을 반드시 입력해주세요!');
+    return;
+  }
+  const tags = [...$tagList.querySelectorAll('.tag')].map($tag => $tag.textContent.substring(1));
   const postBody = {
-    title: document.querySelector('#title').value,
-    tags: [],
+    title: $title.value.trim(),
     content: quill.getContents(),
+    tags,
+    ratings,
   };
   try {
     const { data } = await axios.post('/reviews', postBody);
+    console.log(data);
   } catch (e) {
     console.error(e.message);
   }
 });
 
-document.querySelector('.load').addEventListener('click', async () => {
-  try {
-    const { data } = await axios.get('/reviews/5');
-    const { title, content } = data;
-    document.querySelector('#title').value = title;
-    quill.setContents(content);
-  } catch (e) {
-    console.error(e.message);
-  }
+$tagList.addEventListener('click', e => {
+  const $tag = e.target.closest('.tag');
+  if (!$tag) return;
+
+  $tag.remove();
 });
+
+document.querySelector('.editor-tag').addEventListener('keyup', e => {
+  if (e.key !== 'Enter') return;
+
+  const $li = document.createElement('li');
+  $li.className = 'tag';
+  $li.textContent = `#${e.target.value}`;
+  $tagList.appendChild($li);
+
+  e.target.value = '';
+});
+
+// document.querySelector('.load').addEventListener('click', async () => {
+//   try {
+//     const { data } = await axios.get('/reviews/5');
+//     const { title, content } = data;
+//     document.querySelector('#title').value = title;
+//     quill.setContents(content);
+//   } catch (e) {
+//     console.error(e.message);
+//   }
+// });
